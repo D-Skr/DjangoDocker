ac45c4480588f4c06d32940f9873a94aa3540074
D-Skr <57552518+D-Skr@users.noreply.github.com>
2023-12-02 04:56:14 -0500

    code folder

---
+++ teneast/teneast/static/chunks/pages/website-terms-of-use-21df215ecc5503bf.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6640],{18633:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/website-terms-of-use",function(){return e(93589)}])},93589:function(n,u,e){"use strict";e.r(u),e.d(u,{Terms:function(){return o}});var t=e(85893),r=e(11163),s=e(33532),o=function(){var n=(0,r.useRouter)().pathname;return(0,t.jsx)(s.Z,{route:n})};u.default=o}},function(n){n.O(0,[8780,4623,9774,2888,179],(function(){return u=18633,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
+//# sourceMappingURL=website-terms-of-use-21df215ecc5503bf.js.map
